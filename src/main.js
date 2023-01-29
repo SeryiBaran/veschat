36d2182ff730f42946152eb685dd282461e73eff
SeryiBaran <seryibaranminepy@yandex.ru>
2023-01-29 20:29:34 +0300

    remove pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import 'virtual:windi.css'
 import './assets/main.css'
 
 const app = createApp(App)
-
-app.use(createPinia())
 
 app.mount('#app')
