528c3558fab03bbc6b6fd7fbbefb25330ef29379
SeryiBaran <seryibaranminepy@yandex.ru>
2023-03-13 11:57:28 +0300

    npm-run-all

--- server/controllers/messages.controller.ts
+++ server/controllers/messages.controller.ts
@@ -3,6 +3,6 @@ import { messagesApi } from '../api';
 export const messagesController = new (class {
   async getAllMessages(_, res) {
     const messages = await messagesApi.getAll();
-    res.status(200).send({ messages });
+    res.status(200).json({ messages });
   }
 })();
